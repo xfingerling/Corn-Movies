31f0424d46676e5259a197a66438c7906315e086
Oleg Zavhorodnii <oleggenius95gmail.com>
2020-05-11 17:55:44 +0300

    Code Splitting

--- src/Components/MoviesApp/MoviesApp.jsx
+++ src/Components/MoviesApp/MoviesApp.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 
+import MyLoader from "../Loader/Loader";
 import Navigation from "../../Components/Navigation/Navigation";
-import HomePage from "../../pages/HomePage/HomePage";
-import MoviesPage from "../../pages/MoviesPage/MoviesPage";
-import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
-import NotFoundPage from "../NotFoundPage/NotFoundPage";
+
+const HomePage = lazy(() =>
+  import("../../pages/HomePage/HomePage" /* webpackChunkName: "home-page" */),
+);
+
+const MoviesPage = lazy(() =>
+  import(
+    "../../pages/MoviesPage/MoviesPage" /* webpackChunkName: "movies-page" */
+  ),
+);
+
+const MovieDetailsPage = lazy(() =>
+  import(
+    "../../pages/MovieDetailsPage/MovieDetailsPage" /* webpackChunkName: "movie-details-page" */
+  ),
+);
+
+const NotFoundPage = lazy(() =>
+  import(
+    "../../pages/NotFoundPage/NotFoundPage" /* webpackChunkName: "not-found-page" */
+  ),
+);
 
 const MoviesApp = () => {
   return (
     <div>
       <Navigation />
 
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/movies/:id" component={MovieDetailsPage} />
-        <Route path="/movies" exact component={MoviesPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Suspense fallback={MyLoader}>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/movies/:id" component={MovieDetailsPage} />
+          <Route path="/movies" exact component={MoviesPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Suspense>
     </div>
   );
 };
